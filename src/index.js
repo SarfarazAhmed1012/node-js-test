36671cf193c1c6a30437ca0c518e0c84bbad9781
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2024-07-08 00:39:49 +0500

    utils code (asyncHandler)

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: "./env"
 })
-const app = express()
 
-connectDB()
+const app = express()
+const PORT = process.env.PORT || 8000
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port: ${PORT}`)
+    })
+}).catch((err) => {
+    console.log(`Mongo DB connection failed !!! ${err}`)
+})
 
 
 
